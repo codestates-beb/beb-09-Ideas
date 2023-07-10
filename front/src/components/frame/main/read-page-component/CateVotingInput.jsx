c20ed39dd027e3bfb70fb1b11277e3ec8b574990
이승민 <zeroinmars@gmail.com>
2023-07-10 20:03:06 +0900

    Update voting input modified

--- front/src/components/frame/main/read-page-component/CateVotingInput.jsx
+++ front/src/components/frame/main/read-page-component/CateVotingInput.jsx
@@ -4,18 +4,30 @@ import {MenuItem, FormControl, Select, InputLabel} from '@mui/material/';
 
 const CateVoting = styled.div`
     display: ${({ isVisible, isFirstInput }) => (isFirstInput?'block':(isVisible ? 'block' : 'none'))};
-    margin-bottom: 50px;
+    margin-bottom: 20px;
 
     /* display:none;    */
+    button {
+        position:absolute;
+        border: 0;
+        font-size: 15px;
+        background: transparent;
+        &:hover {
+            background: #f1f1f1;
+        }
+    }
     `;
+const RatioInput = styled.input`
+    height:35px;
+    width:45px;
+`;
 
 const CateVotingInput = ({isVisible, isFirstInput, addIndex, deleteIndex}) => {
-    console.log(isVisible);
     
   return (
     <CateVoting isVisible={isVisible} isFirstInput={isFirstInput}>
-          <FormControl sx={{ m: 0, minWidth: 150 }}>
-            <InputLabel htmlFor="category">category</InputLabel>
+          <FormControl size='small' sx={{ m: 0, minWidth: 150 }}>
+            <InputLabel size='small' htmlFor="category">category</InputLabel>
             <Select defaultValue="" id="category-select" label="category">
               <MenuItem onClick={addIndex} value={"Manage"}>Manage</MenuItem>
               <MenuItem onClick={addIndex} value={"Economy"}>Economy</MenuItem>
@@ -25,7 +37,7 @@ const CateVotingInput = ({isVisible, isFirstInput, addIndex, deleteIndex}) => {
               <MenuItem onClick={addIndex} value={"Cloud"}>Cloud</MenuItem>
             </Select>
           </FormControl>
-            <input type="text" />
+            <RatioInput type="text" />
             {isFirstInput?"":<button onClick={deleteIndex}> x </button>}
     </CateVoting>
   )
