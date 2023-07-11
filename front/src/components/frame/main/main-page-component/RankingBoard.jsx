f8b64669017df6d3b7b17ad1b16311c3fa35780b
이승민 <zeroinmars@gmail.com>
2023-07-11 11:35:45 +0900

    Update token info, rank updated

--- front/src/components/frame/main/main-page-component/RankingBoard.jsx
+++ front/src/components/frame/main/main-page-component/RankingBoard.jsx
@@ -4,11 +4,27 @@ import styled from 'styled-components';
 const RankingBoardDiv = styled.div`
     height:100px;
     width:370px;
-    border: 1px solid black;
-    border-radius: 15px;
     margin: auto;
-    margin-top: 40px;
-    margin-bottom: 40px;
+    margin-top: 15px;
+    margin-bottom: 15px;
+    /* border: 1px dotted black;
+    border-radius: 20px 5px 20px 5px; */
+`;
+const RankingTopDiv = styled.div`
+      display:grid;
+    grid-template-columns: 50px 70px 1fr;
+    align-items: center;
+    justify-items: center;
+`;
+
+const RankingBottomDiv = styled.div`
+      display:grid;
+        grid-template-columns: 50px 70px 1fr 1fr 1fr;
+        align-items: center;
+        justify-items: center;
+    span {
+        font-size: 12px;
+    }
     
 `;
 
@@ -17,15 +33,30 @@ const CircleDiv = styled.div`
     border-radius: 50px;
     height: 30px;
     width: 30px;
-    span {
-        
+    display:flex;
+    align-items: center;
+    div {
+        margin:auto;
     }
+    
 `;
 
-const RankingBoard = () => {
+const RankingBoard = ({rankingNumber}) => {
   return (
     <RankingBoardDiv>
-        <CircleDiv><span>1</span></CircleDiv>
+        <hr/>
+        <RankingTopDiv>
+            <CircleDiv><div>{rankingNumber}</div></CircleDiv>
+            <img src="" alt="img" />
+            <h4>user name</h4>
+        </RankingTopDiv>
+        <RankingBottomDiv>
+            <div></div>
+            <span>follower : <br/>300</span>
+            <span>total : 181</span>
+            <span>AI :57</span>
+            <span>Security : 32</span>
+        </RankingBottomDiv>
     </RankingBoardDiv>
   )
 }
