74607c76d764e06eb2eb0a9fae799ef9c9492eec
이승민 <zeroinmars@gmail.com>
2023-07-11 23:47:45 +0900

    Update UI and voting function modified in reading page

--- front/src/components/frame/main/read-page-component/Comment.jsx
+++ front/src/components/frame/main/read-page-component/Comment.jsx
@@ -1,17 +1,6 @@
 import React, { useState } from 'react'
 import styled from '@emotion/styled'
-import TextField from '@mui/material/TextField';
 
-const CommentView = styled.div`
-  margin: 25px;
-  padding: 10px;
-  margin-top: 50px;
-  text-align: left;
-`;
-
-const TextView = styled.div`
-  margin-top: 15px;
-`;
 
 const NameTimeView = styled.div`
    margin: 10px;
@@ -46,40 +35,26 @@ const Comment = () => {
   }
 
   return (
-    <CommentView>
-      87 comment
-      <TextView>
-        <TextField
-          label="Comment"
-          required
-          fullWidth
-          id="name"
-          value={userComment}
-          onChange={e => setUserComment(e.target.value)}
-        />
-
-      </TextView>
-
-      <hr />
+    <>
+        <hr />
         <NameTimeView>
         coding kim 2hour ago
         </NameTimeView>
 
         <DescriptionView>
         The vanishing and exploding gradient phenomena are often encountered in the context of RNNs. The reason why they happen is that it is difficult to capture long term dependencies because of multiplicative gradient that can be exponentially decreasing/increasing with respect to the number of layers.
-    
+
         </DescriptionView>
-    
+
         <CehckView>
         3 comment
         <ArrangementDiv>
-        <button onClick={handleLike}>좋아요</button><p>{likeCount}</p>
-        <button onClick={handleDislike}>싫어요</button>  <p>{dislikeCount}</p>
+            <button onClick={handleLike}>좋아요</button><p>{likeCount}</p>
+            <button onClick={handleDislike}>싫어요</button>  <p>{dislikeCount}</p>
         </ArrangementDiv>
-    
-        </CehckView>
 
-    </CommentView>
+        </CehckView>
+    </>
 
 
   )
--- front/src/components/frame/main/read-page-component/Description.jsx
+++ front/src/components/frame/main/read-page-component/Description.jsx
@@ -2,21 +2,39 @@ import React from 'react'
 import styled from '@emotion/styled'
 
 const DescriptionDiv = styled.div`
+    width:80%;
+    margin:auto;
+    text-align: left;
+    font-size: 20px;
+    margin-top: 80px;
+    margin-bottom: 100px;
     
-    text-align: center;
 `;
 
 const Description = () => {
   return (
-    <div>
-      <DescriptionDiv>
-      This is the second article in a series dedicated to Deep Learning, a group of Machine Learning methods that has its roots dating back to the 1940’s. Deep Learning gained attention in the last decades for its groundbreaking applications in areas like image classification, speech recognition, and machine translation
+    <DescriptionDiv>
+    This is the second article in a series dedicated to Deep Learning, a group of Machine Learning methods that has its roots dating back to the 1940’s. Deep Learning gained attention in the last decades for its groundbreaking applications in areas like image classification, speech recognition, and machine translation
+    But lots of real-world problems involve a time dimension. What about when the problem at hand comes in the form of a sequence?
+    This is the second article in a series dedicated to Deep Learning, a group of Machine Learning methods that has its roots dating back to the 1940’s. Deep Learning gained attention in the last decades for its groundbreaking applications in areas like image classification, speech recognition, and machine translation
+    But lots of real-world problems involve a time dimension. What about when the problem at hand comes in the form of a sequence?
+    This is the second article in a series dedicated to Deep Learning, a group of Machine Learning methods that has its roots dating back to the 1940’s. Deep Learning gained attention in the last decades for its groundbreaking applications in areas like image classification, speech recognition, and machine translation
+    But lots of real-world problems involve a time dimension. What about when the problem at hand comes in the form of a sequence?
+    This is the second article in a series dedicated to Deep Learning, a group of Machine Learning methods that has its roots dating back to the 1940’s. Deep Learning gained attention in the last decades for its groundbreaking applications in areas like image classification, speech recognition, and machine translation
+    But lots of real-world problems involve a time dimension. What about when the problem at hand comes in the form of a sequence?
+    This is the second article in a series dedicated to Deep Learning, a group of Machine Learning methods that has its roots dating back to the 1940’s. Deep Learning gained attention in the last decades for its groundbreaking applications in areas like image classification, speech recognition, and machine translation
+    But lots of real-world problems involve a time dimension. What about when the problem at hand comes in the form of a sequence?
+    This is the second article in a series dedicated to Deep Learning, a group of Machine Learning methods that has its roots dating back to the 1940’s. Deep Learning gained attention in the last decades for its groundbreaking applications in areas like image classification, speech recognition, and machine translation
+    But lots of real-world problems involve a time dimension. What about when the problem at hand comes in the form of a sequence?
+    This is the second article in a series dedicated to Deep Learning, a group of Machine Learning methods that has its roots dating back to the 1940’s. Deep Learning gained attention in the last decades for its groundbreaking applications in areas like image classification, speech recognition, and machine translation
+    But lots of real-world problems involve a time dimension. What about when the problem at hand comes in the form of a sequence?
+    This is the second article in a series dedicated to Deep Learning, a group of Machine Learning methods that has its roots dating back to the 1940’s. Deep Learning gained attention in the last decades for its groundbreaking applications in areas like image classification, speech recognition, and machine translation
+    But lots of real-world problems involve a time dimension. What about when the problem at hand comes in the form of a sequence?
+    This is the second article in a series dedicated to Deep Learning, a group of Machine Learning methods that has its roots dating back to the 1940’s. Deep Learning gained attention in the last decades for its groundbreaking applications in areas like image classification, speech recognition, and machine translation
+    But lots of real-world problems involve a time dimension. What about when the problem at hand comes in the form of a sequence?
 
-But lots of real-world problems involve a time dimension. What about when the problem at hand comes in the form of a sequence?
-
-.......
-      </DescriptionDiv>
-    </div>
+    </DescriptionDiv>
+    
   )
 }
 
