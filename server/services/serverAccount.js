f023c15d9a7b3e842baa0f731975d20c3a2a1002
breadmore <lepo3@naver.com>
2023-07-12 16:08:19 +0900

    fix contract

--- server/services/serverAccount.js
+++ server/services/serverAccount.js
@@ -1,23 +1,14 @@
 import User from "./../models/user.js";
-<<<<<<< HEAD
-import Wallet from "../models/wallet.js";
 import {createServerAccount, createUserDistributedWallet} from "./wallet.js";
 import {divideTokenToUDW} from "./contract.js";
+
 export async function initServer(){
-    let isServer =await  User.find({id:"ideas"});
-=======
-import { ethers } from "ethers";
-import Wallet from "../models/wallet.js";
-export async function initServer() {
-  let isServer = await User.find({ id: "ideas" });
->>>>>>> 7856ed958955421960f6c3d19bf0d8e7060ce084
+    let isServer =await User.find({id:"ideas"});
 
-  if (isServer.length !== 0) {
-    console.log("Exist server");
-    return;
-  }
-
-<<<<<<< HEAD
+    if (isServer.length !== 0) {
+        console.log("Exist server");
+        return;
+    }
     const userData = {
         "id": "ideas",
         "email": "ideas@gmail.com",
@@ -45,30 +36,3 @@ export async function initServer() {
     serverWalletForDB.save();
     await divideTokenToUDW(await createUserDistributedWallet());
 }
-=======
-  const userData = {
-    id: "ideas",
-    email: "ideas@gmail.com",
-    user_name: "server",
-    password: "goodideas",
-    phoneNum: "82 10-1234-0101",
-    role: 1,
-  };
-
-  const serverAccount = new User(userData);
-
-  console.log("서버생성");
-  const provider = new ethers.JsonRpcProvider("http://127.0.0.1:7545");
-  const addressList = await provider.listAccounts();
-  const serverAddress = addressList[0].address;
-  const serverData = {
-    userId: "ideas",
-    address: serverAddress,
-  };
-
-  const serverWallet = new Wallet(serverData);
-
-  serverAccount.save();
-  serverWallet.save();
-}
->>>>>>> 7856ed958955421960f6c3d19bf0d8e7060ce084
